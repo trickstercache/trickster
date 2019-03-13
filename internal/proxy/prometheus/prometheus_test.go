f06a79df0cafab58460b113c6e71d7dc805eec2f
James Ranson <james_ranson@cable.comcast.com>
2019-03-13 10:25:50 -0600

    Add a few prometheus tests

--- internal/proxy/prometheus/prometheus_test.go
+++ internal/proxy/prometheus/prometheus_test.go
@@ -13,25 +13,27 @@
 
 package prometheus
 
-import (
-	"testing"
-	"time"
-)
+import "testing"
 
-func TestSetStep(t *testing.T) {
-	me := MatrixEnvelope{}
-	const step = time.Duration(300) * time.Minute
-	me.SetStep(step)
-	if me.StepDuration != step {
-		t.Errorf(`wanted "%s". got "%s"`, step, me.StepDuration)
+func TestParseTime(t *testing.T) {
+	fixtures := []struct {
+		input  string
+		output string
+	}{
+		{"2018-04-07T05:08:53.200Z", "2018-04-07 05:08:53.2 +0000 UTC"},
+		{"1523077733", "2018-04-07 05:08:53 +0000 UTC"},
+		{"1523077733.2", "2018-04-07 05:08:53.2 +0000 UTC"},
 	}
-}
 
-func TestStep(t *testing.T) {
-	me := MatrixEnvelope{}
-	const step = time.Duration(300) * time.Minute
-	me.SetStep(step)
-	if me.Step() != step {
-		t.Errorf(`wanted "%s". got "%s"`, step, me.Step())
+	for _, f := range fixtures {
+		out, err := parseTime(f.input)
+		if err != nil {
+			t.Error(err)
+		}
+
+		outStr := out.UTC().String()
+		if outStr != f.output {
+			t.Errorf("Expected %s, got %s for input %s", f.output, outStr, f.input)
+		}
 	}
 }
