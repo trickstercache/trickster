d3f108cce1f272bfe1e4fe928baf625091df2393
James Ranson <james_ranson@cable.comcast.com>
2019-03-13 08:39:49 -0600

    Add Headers Tests

--- internal/proxy/headers_test.go
+++ internal/proxy/headers_test.go
@@ -12,3 +12,63 @@
  */
 
 package proxy
+
+import (
+	"net/http"
+	"strings"
+	"testing"
+
+	"github.com/Comcast/trickster/internal/config"
+)
+
+func TestAddProxyHeaders(t *testing.T) {
+
+	headers := http.Header{}
+	config.ApplicationName = "trickster-test"
+	config.ApplicationVersion = "tests"
+
+	addProxyHeaders("0.0.0.0", headers)
+
+	if _, ok := headers[hnXForwardedFor]; !ok {
+		t.Errorf("missing header %s", hnXForwardedFor)
+	}
+
+	if _, ok := headers[hnXForwardedBy]; !ok {
+		t.Errorf("missing header %s", hnXForwardedBy)
+	}
+
+}
+
+func TestExractHeader(t *testing.T) {
+
+	headers := http.Header{}
+
+	const appName = "trickster-test"
+	const appVer = "tests"
+	const appString = appName + " " + appVer
+
+	config.ApplicationName = appName
+	config.ApplicationVersion = appVer
+
+	const testIP = "0.0.0.0"
+
+	addProxyHeaders(testIP, headers)
+
+	if h, ok := headers[hnXForwardedFor]; !ok {
+		t.Errorf("missing header %s", hnXForwardedFor)
+	} else {
+		v := strings.Join(h, "")
+		if v != testIP {
+			t.Errorf(`wanted "%s". got "%s"`, testIP, v)
+		}
+	}
+
+	if h, ok := headers[hnXForwardedBy]; !ok {
+		t.Errorf("missing header %s", hnXForwardedBy)
+	} else {
+		v := strings.Join(h, "")
+		if v != appString {
+			t.Errorf(`wanted "%s". got "%s"`, appString, v)
+		}
+	}
+}
