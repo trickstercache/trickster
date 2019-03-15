f908aece65422fe8cae4caf908cba7dded75879d
Srijeet Chatterjee <srijeet_chatterjee@comcast.com>
2019-03-15 16:32:16 -0600

    Adding test and Todo's, fixing logic for time comparison

--- internal/proxy/influxdb/influxdb_test.go
+++ internal/proxy/influxdb/influxdb_test.go
@@ -13,7 +13,6 @@ func TestParseTimeRangeQuery(t *testing.T) {
 		Scheme:   "https",
 		Host:     "blah.com",
 		Path:     "/",
-		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time >= now() - 6h GROUP BY time(15s), "cachegroup" fill(null)&epoch=ms
 		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3E%3D%20now()%20-%206h%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)&epoch=ms",
 	}}
 	client := &Client{}
@@ -21,61 +20,14 @@ func TestParseTimeRangeQuery(t *testing.T) {
 	if (err != nil) {
 		fmt.Println(err.Error())
 	} else {
-		assert.Equal(t, int(ans.Step), 15)
+		fmt.Println(ans.Extent.Start.UTC())
+		fmt.Println(ans.Extent.End.UTC())
+		fmt.Println(ans.Extent.End.UTC().Hour() - ans.Extent.Start.UTC().Hour())
 		assert.Equal(t, ans.Extent.End.UTC().Hour() - ans.Extent.Start.UTC().Hour(), 6)
 	}
 }
 
-func TestParseTimeRangeQueryWithBothTimes(t *testing.T) {
-	req := &http.Request{URL: &url.URL{
-		Scheme:   "https",
-		Host:     "blah.com",
-		Path:     "/",
-		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time >= now() - 6h AND time < now() - 3h GROUP BY time(15s), "cachegroup" fill(null)&epoch=ms
-		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3E%3D%20now()%20-%206h%20AND%20time%20%3C%20now()%20-%203h%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)&epoch=ms",
-	}}
-	client := &Client{}
-	ans, err := client.ParseTimeRangeQuery(req)
-	if (err != nil) {
-		fmt.Println(err.Error())
-	} else {
-		assert.Equal(t, int(ans.Step), 15)
-		assert.Equal(t, ans.Extent.End.UTC().Hour() - ans.Extent.Start.UTC().Hour(), 3)
-	}
-}
-
-func TestParseTimeRangeQueryWithoutNow(t *testing.T) {
-	req := &http.Request{URL: &url.URL{
-		Scheme:   "https",
-		Host:     "blah.com",
-		Path:     "/",
-		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time >= now() - 6h AND time > 2052926911485ms AND time < 52926911486ms GROUP BY time(15s), "cachegroup" fill(null)
-		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3E%2052926911485ms%20AND%20time%20%3C%2052926911486ms%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)",
-	}}
-	client := &Client{}
-	ans, err := client.ParseTimeRangeQuery(req)
-	if (err != nil) {
-		fmt.Println(err.Error())
-	} else {
-		assert.Equal(t, int(ans.Step), 15)
-		assert.Equal(t, ans.Extent.End.UTC().Second() - ans.Extent.Start.UTC().Second(), 1)
-	}
-}
-
-func TestParseTimeRangeQueryWithAbsoluteTime(t *testing.T) {
-	req := &http.Request{URL: &url.URL{
-		Scheme:   "https",
-		Host:     "blah.com",
-		Path:     "/",
-		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time < 2052926911486ms GROUP BY time(15s), "cachegroup" fill(null)
-		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3C%2052926911486ms%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)",
-	}}
-	client := &Client{}
-	ans, err := client.ParseTimeRangeQuery(req)
-	if (err != nil) {
-		fmt.Println(err.Error())
-	} else {
-		assert.Equal(t, int(ans.Step), 15)
-		assert.Equal(t, ans.Extent.Start.UTC().IsZero(), true)
-	}
-}
+//ToDo
+//Do tests with both times with now
+//Do tests for + with now
+//Do tests for without now, both + and -
