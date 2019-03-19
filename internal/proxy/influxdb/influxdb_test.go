34871be933f15388c630ee69e35fbd0959664f81
Srijeet Chatterjee <srijeet_chatterjee@comcast.com>
2019-03-19 10:04:22 -0600

    Adding comment

--- internal/proxy/influxdb/influxdb_test.go
+++ internal/proxy/influxdb/influxdb_test.go
@@ -13,6 +13,7 @@ func TestParseTimeRangeQuery(t *testing.T) {
 		Scheme:   "https",
 		Host:     "blah.com",
 		Path:     "/",
+		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time >= now() - 6h GROUP BY time(15s), "cachegroup" fill(null)&epoch=ms
 		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3E%3D%20now()%20-%206h%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)&epoch=ms",
 	}}
 	client := &Client{}
@@ -30,6 +31,7 @@ func TestParseTimeRangeQueryWithBothTimes(t *testing.T) {
 		Scheme:   "https",
 		Host:     "blah.com",
 		Path:     "/",
+		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time >= now() - 6h AND time < now() - 3h GROUP BY time(15s), "cachegroup" fill(null)&epoch=ms
 		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3E%3D%20now()%20-%206h%20AND%20time%20%3C%20now()%20-%203h%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)&epoch=ms",
 	}}
 	client := &Client{}
@@ -47,6 +49,7 @@ func TestParseTimeRangeQueryWithoutNow(t *testing.T) {
 		Scheme:   "https",
 		Host:     "blah.com",
 		Path:     "/",
+		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time >= now() - 6h AND time > 2052926911485ms AND time < 52926911486ms GROUP BY time(15s), "cachegroup" fill(null)
 		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3E%2052926911485ms%20AND%20time%20%3C%2052926911486ms%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)",
 	}}
 	client := &Client{}
@@ -64,6 +67,7 @@ func TestParseTimeRangeQueryWithAbsoluteTime(t *testing.T) {
 		Scheme:   "https",
 		Host:     "blah.com",
 		Path:     "/",
+		//SELECT mean("value") FROM "monthly"."bandwidth.1min" WHERE ("cdn" = 'over-the-top') AND time < 2052926911486ms GROUP BY time(15s), "cachegroup" fill(null)
 		RawQuery: "q=SELECT%20mean(%22value%22)%20FROM%20%22monthly%22.%22bandwidth.1min%22%20WHERE%20(%22cdn%22%20%3D%20%27over-the-top%27)%20AND%20time%20%3C%2052926911486ms%20GROUP%20BY%20time(15s)%2C%20%22cachegroup%22%20fill(null)",
 	}}
 	client := &Client{}
