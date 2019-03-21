e35d134fc214950988b5f3098b536f2e06e67154
James Ranson <james_ranson@cable.comcast.com>
2019-03-21 05:32:04 -0600

    add Crop() test

--- internal/proxy/prometheus/matrix_test.go
+++ internal/proxy/prometheus/matrix_test.go
@@ -14,8 +14,13 @@
 package prometheus
 
 import (
+	"reflect"
+	"strconv"
 	"testing"
 	"time"
+
+	"github.com/Comcast/trickster/internal/timeseries"
+	"github.com/prometheus/common/model"
 )
 
 func TestSetStep(t *testing.T) {
@@ -33,5 +38,257 @@ func TestStep(t *testing.T) {
 	me.SetStep(step)
 	if me.Step() != step {
 		t.Errorf(`wanted "%s". got "%s"`, step, me.Step())
+	}
+}
+
+func TestMerge(t *testing.T) {
+
+}
+
+func TestCrop(t *testing.T) {
+	tests := []struct {
+		before, after *MatrixEnvelope
+		extent        timeseries.Extent
+	}{
+		// Case where we trim nothing
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "a"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 1544004600000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "a"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 1544004600000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(0, 0),
+				End:   time.Unix(1644004600, 0),
+			},
+		},
+		// Case where we trim everything (all data is too late)
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "b"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 1544004600000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "b"},
+							Values: []model.SamplePair{},
+						}},
+				},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(0, 0),
+				End:   time.Unix(10, 0),
+			},
+		},
+		// Case where we trim everything (all data is too early)
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "c"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 100000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "c"},
+							Values: []model.SamplePair{},
+						},
+					}},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(10000, 0),
+				End:   time.Unix(20000, 0),
+			},
+		},
+		// Case where we trim some off the beginning
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "d"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 99000, Value: 1.5},
+								model.SamplePair{Timestamp: 199000, Value: 1.5},
+								model.SamplePair{Timestamp: 299000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "d"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 299000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(200, 0),
+				End:   time.Unix(300, 0),
+			},
+		},
+		// Case where we trim some off the end
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "e"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 99000, Value: 1.5},
+								model.SamplePair{Timestamp: 199000, Value: 1.5},
+								model.SamplePair{Timestamp: 299000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "e"},
+							Values: []model.SamplePair{
+								model.SamplePair{Timestamp: 199000, Value: 1.5},
+							},
+						},
+					},
+				},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(100, 0),
+				End:   time.Unix(200, 0),
+			},
+		},
+
+		// Case where we aren't given any datapoints
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "f"},
+							Values: []model.SamplePair{},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "f"},
+							Values: []model.SamplePair{},
+						},
+					},
+				},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(200, 0),
+				End:   time.Unix(300, 0),
+			},
+		},
+
+		// Case where we have more series than points
+		{
+			before: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "g"},
+							Values: []model.SamplePair{model.SamplePair{Timestamp: 99000, Value: 1.5}},
+						},
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "g"},
+							Values: []model.SamplePair{model.SamplePair{Timestamp: 99000, Value: 1.5}},
+						},
+					},
+				},
+			},
+			after: &MatrixEnvelope{
+				Data: MatrixData{
+					ResultType: "matrix",
+					Result: model.Matrix{
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "g"},
+							Values: []model.SamplePair{},
+						},
+						&model.SampleStream{
+							Metric: model.Metric{"__name__": "g"},
+							Values: []model.SamplePair{},
+						},
+					},
+				},
+			},
+			extent: timeseries.Extent{
+				Start: time.Unix(200, 0),
+				End:   time.Unix(300, 0),
+			},
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			result := test.before.Crop(test.extent).(*MatrixEnvelope)
+			if !reflect.DeepEqual(result, test.after) {
+				t.Fatalf("mismatch\nexpected=%v\nactual=%v", test.after, result)
+			}
+		})
 	}
 }
