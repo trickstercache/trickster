3b394545d89f2e62d672a51c2c5465cbee4acf06
James Ranson <james_ranson@cable.comcast.com>
2019-03-21 11:02:10 -0600

    cleanup log tests

--- internal/util/log/logging_test.go
+++ internal/util/log/logging_test.go
@@ -14,10 +14,13 @@
 package log
 
 import (
+	"os"
 	"testing"
+
+	"github.com/Comcast/trickster/internal/config"
 )
 
-func TestNewLogger(t *testing.T) {
+func TestConsoleLogger(t *testing.T) {
 	testCases := []string{
 		"debug",
 		"info",
@@ -28,12 +31,23 @@ func TestNewLogger(t *testing.T) {
 	// it should create a logger for each level
 	for _, tc := range testCases {
 		t.Run(tc, func(t *testing.T) {
-			newLogger(LoggingConfig{LogLevel: tc}, tc)
+			l := ConsoleLogger(tc)
+			if l.level != tc {
+				t.Errorf("mismatch in log level: expected=%s actual=%s", tc, l.level)
+			}
 		})
 	}
 }
 
 func TestNewLogger_LogFile(t *testing.T) {
 	// it should create a logger that outputs to a log file ("out.test.log")
-	newLogger(LoggingConfig{LogFile: "out.log"}, "test")
+	config.Config = config.NewConfig()
+	config.Main = &config.MainConfig{InstanceID: 0}
+	config.Logging = &config.LoggingConfig{LogFile: "out.log", LogLevel: "info"}
+	Init()
+	Info("test entry", Pairs{"testKey": "testVal"})
+	if _, err := os.Stat("out.log"); err != nil {
+		t.Errorf(err.Error())
+	}
+
 }
