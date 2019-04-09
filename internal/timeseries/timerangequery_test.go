9c500abd4f9009a7457499342d60f3f2a1420b89
James Ranson <james@ranson.org>
2019-04-08 20:10:51 -0600

    1.0 cache tests (#192)
    * add cache registration tests
    
    * add route registration test wip
    
    * add empty extent test case
    
    * Add TestCalculateDeltas

--- internal/timeseries/timerangequery_test.go
+++ internal/timeseries/timerangequery_test.go
@@ -19,7 +19,59 @@ import (
 	"time"
 )
 
+func TestCalculateDeltas(t *testing.T) {
+
+	tests := []struct {
+		have               []Extent
+		expected           []Extent
+		start, end, stepMS int64
+	}{
+		{
+			[]Extent{Extent{Start: time.Unix(50, 0), End: time.Unix(100, 0)}},
+			[]Extent{Extent{Start: time.Unix(1, 0), End: time.Unix(49, 0)}},
+			1, 100, 1,
+		},
+		{
+			[]Extent{Extent{Start: time.Unix(50, 0), End: time.Unix(100, 0)}},
+			[]Extent{Extent{Start: time.Unix(1, 0), End: time.Unix(49, 0)}, Extent{Start: time.Unix(101, 0), End: time.Unix(101, 0)}},
+			1, 101, 1,
+		},
+		{
+			[]Extent{Extent{Start: time.Unix(1, 0), End: time.Unix(100, 0)}},
+			[]Extent{Extent{Start: time.Unix(101, 0), End: time.Unix(101, 0)}},
+			1, 101, 1,
+		},
+	}
+
+	for i, test := range tests {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+
+			trq := TimeRangeQuery{Statement: "up", Extent: Extent{Start: time.Unix(test.start, 0), End: time.Unix(test.end, 0)}, Step: test.stepMS}
+			trq.NormalizeExtent()
+			d := trq.CalculateDeltas(test.have)
+
+			if len(d) != len(test.expected) {
+				t.Errorf("expected %v got %v", test.expected, d)
+				return
+			}
+
+			for i := range d {
+				if d[i].Start != test.expected[i].Start {
+					t.Errorf("expected %d got %d", test.expected[i].Start.Unix(), d[i].Start.Unix())
+				}
+				if d[i].End != test.expected[i].End {
+					t.Errorf("expected %d got %d", test.expected[i].End.Unix(), d[i].End.Unix())
+				}
+			}
+		})
+	}
+}
+
 func TestNormalizeExtent(t *testing.T) {
+
+	tmrw := time.Now().Add(time.Duration(24) * time.Hour).Unix()
+	expected := (time.Now().Unix() / 10) * 10
+
 	tests := []struct {
 		start, end, stepMS, now int64
 		rangeStart, rangeEnd    int64
@@ -35,6 +87,12 @@ func TestNormalizeExtent(t *testing.T) {
 		{
 			1, 103, 10, 1,
 			0, 100,
+			false,
+		},
+		// Ensure that it brings in future times
+		{
+			1, tmrw, 10, 1,
+			0, expected,
 			false,
 		},
 	}
