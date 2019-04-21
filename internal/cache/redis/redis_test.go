13c48527a8ed84379c8a8a88a5deef5ece994175
James Ranson <james@ranson.org>
2019-04-21 16:19:38 -0600

    Update redis_test.go

--- internal/cache/redis/redis_test.go
+++ internal/cache/redis/redis_test.go
@@ -1,5 +1,5 @@
 /**
-* Copyright 2018 Corcast Cable Communications Management, LLC
+* Copyright 2018 Comcast Cable Communications Management, LLC
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
