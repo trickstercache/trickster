d1795c4e61e4649a437fa5308e7e03fb48707e9e
James Ranson <james_ranson@cable.comcast.com>
2019-04-21 09:53:40 -0600

    additional memory cache tests

--- internal/cache/memory/memory_test.go
+++ internal/cache/memory/memory_test.go
@@ -25,9 +25,21 @@ func init() {
 	metrics.Init()
 }
 
+const cacheType = "memory"
+const cacheKey = "cacheKey"
+
+func TestConfiguration(t *testing.T) {
+	cacheConfig := config.CachingConfig{Type: cacheType, BBolt: config.BBoltCacheConfig{Filename: "/tmp/test.db", Bucket: "trickster_test"}, Index: config.CacheIndexConfig{ReapInterval: time.Second}}
+	mc := Cache{Config: &cacheConfig}
+	cfg := mc.Configuration()
+	if cfg.Type != cacheType {
+		t.Fatalf("expected %s got %s", cacheType, cfg.Type)
+	}
+}
+
 func TestCache_Connect(t *testing.T) {
 
-	cacheConfig := config.CachingConfig{Type: "memory", Index: config.CacheIndexConfig{ReapInterval: 0}}
+	cacheConfig := config.CachingConfig{Type: cacheType, Index: config.CacheIndexConfig{ReapInterval: 0}}
 	mc := Cache{Config: &cacheConfig}
 
 	// it should connect
@@ -38,7 +50,7 @@ func TestCache_Connect(t *testing.T) {
 }
 
 func TestCache_Store(t *testing.T) {
-	cacheConfig := config.CachingConfig{Type: "memory", Index: config.CacheIndexConfig{ReapInterval: 0}}
+	cacheConfig := config.CachingConfig{Type: cacheType, Index: config.CacheIndexConfig{ReapInterval: 0}}
 	mc := Cache{Config: &cacheConfig}
 
 	err := mc.Connect()
@@ -47,7 +59,7 @@ func TestCache_Store(t *testing.T) {
 	}
 
 	// it should store a value
-	err = mc.Store("cacheKey", []byte("data"), time.Duration(60)*time.Second)
+	err = mc.Store(cacheKey, []byte("data"), time.Duration(60)*time.Second)
 	if err != nil {
 		t.Error(err)
 	}
@@ -55,7 +67,7 @@ func TestCache_Store(t *testing.T) {
 
 func TestCache_Retrieve(t *testing.T) {
 
-	cacheConfig := config.CachingConfig{Type: "memory", Index: config.CacheIndexConfig{ReapInterval: 0}}
+	cacheConfig := config.CachingConfig{Type: cacheType, Index: config.CacheIndexConfig{ReapInterval: 0}}
 	mc := Cache{Config: &cacheConfig}
 
 	err := mc.Connect()
@@ -63,14 +75,14 @@ func TestCache_Retrieve(t *testing.T) {
 		t.Error(err)
 	}
 
-	err = mc.Store("cacheKey", []byte("data"), time.Duration(60)*time.Second)
+	err = mc.Store(cacheKey, []byte("data"), time.Duration(60)*time.Second)
 	if err != nil {
 		t.Error(err)
 	}
 
 	// it should retrieve a value
 	var data []byte
-	data, err = mc.Retrieve("cacheKey")
+	data, err = mc.Retrieve(cacheKey)
 	if err != nil {
 		t.Error(err)
 	}
@@ -80,7 +92,79 @@ func TestCache_Retrieve(t *testing.T) {
 }
 
 func TestCache_Close(t *testing.T) {
-	cacheConfig := config.CachingConfig{Type: "memory", Index: config.CacheIndexConfig{ReapInterval: time.Second}}
+	cacheConfig := config.CachingConfig{Type: cacheType, Index: config.CacheIndexConfig{ReapInterval: time.Second}}
 	mc := Cache{Config: &cacheConfig}
 	mc.Close()
+}
+
+func TestCache_Remove(t *testing.T) {
+
+	cacheConfig := config.CachingConfig{Type: cacheType, Index: config.CacheIndexConfig{ReapInterval: time.Second}}
+	mc := Cache{Config: &cacheConfig}
+
+	err := mc.Connect()
+	if err != nil {
+		t.Error(err)
+	}
+	defer mc.Close()
+
+	// it should store a value
+	err = mc.Store(cacheKey, []byte("data"), time.Duration(60)*time.Second)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// it should retrieve a value
+	data, err := mc.Retrieve(cacheKey)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(data) != "data" {
+		t.Errorf("wanted \"%s\". got \"%s\".", "data", data)
+	}
+
+	mc.Remove(cacheKey)
+
+	// it should be a cache miss
+	data, err = mc.Retrieve(cacheKey)
+	if err == nil {
+		t.Errorf("expected key not found error for %s", cacheKey)
+	}
+
+}
+
+func TestCache_BulkRemove(t *testing.T) {
+
+	cacheConfig := config.CachingConfig{Type: cacheType, Index: config.CacheIndexConfig{ReapInterval: time.Second}}
+	mc := Cache{Config: &cacheConfig}
+
+	err := mc.Connect()
+	if err != nil {
+		t.Error(err)
+	}
+	defer mc.Close()
+
+	// it should store a value
+	err = mc.Store(cacheKey, []byte("data"), time.Duration(60)*time.Second)
+	if err != nil {
+		t.Error(err)
+	}
+
+	// it should retrieve a value
+	data, err := mc.Retrieve(cacheKey)
+	if err != nil {
+		t.Error(err)
+	}
+	if string(data) != "data" {
+		t.Errorf("wanted \"%s\". got \"%s\".", "data", data)
+	}
+
+	mc.BulkRemove([]string{cacheKey}, true)
+
+	// it should be a cache miss
+	data, err = mc.Retrieve(cacheKey)
+	if err == nil {
+		t.Errorf("expected key not found error for %s", cacheKey)
+	}
+
 }
