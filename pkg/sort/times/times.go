97aae86dd38af443302e8a6d6d84aabc3f1a5311
James Ranson <james_ranson@cable.comcast.com>
2019-06-26 12:26:10 -0600

    move sorting types to their own space

---
+++ pkg/sort/times/times.go
@@ -0,0 +1,34 @@
+/**
+* Copyright 2018 Comcast Cable Communications Management, LLC
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package times
+
+import "time"
+
+// Times represents an array of time.Times
+type Times []time.Time
+
+// Len returns the length of an array of time.Times
+func (t Times) Len() int {
+	return len(t)
+}
+
+// Less returns true if i comes before j
+func (t Times) Less(i, j int) bool {
+	return t[i].Before(t[j])
+}
+
+// Swap modifies an array by of time.Times swapping the values in indexes i and j
+func (t Times) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
