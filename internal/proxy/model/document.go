cabc91d04f3d1cd8cd64282731ecbf9ceddeb80a
James Ranson <james_ranson@cable.comcast.com>
2019-09-14 17:33:24 -0600

    String() convenience func for CachingPolicy struct

--- internal/proxy/model/document.go
+++ internal/proxy/model/document.go
@@ -14,6 +14,7 @@
 package model
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -46,6 +47,14 @@ type CachingPolicy struct {
 	IfMatchValue          string    `msg:"if_match_value"`
 	IfModifiedSinceTime   time.Time `msg:"if_modified_since_time"`
 	IfUnmodifiedSinceTime time.Time `msg:"if_unmodified_since_time"`
+}
+
+func (c *CachingPolicy) String() string {
+	return fmt.Sprintf(`{ "is_fresh":%t, "no_cache":%t, "no_transform":%t, "freshness_lifetime":%d, "can_revalidate":%t, "must_revalidate":%t,`+
+		` "last_modified":%d, "expires":%d, "date":%d, "local_date":%d, "etag":"%s", "if_none_match":"%s", "if_match":"%s",`+
+		` "if_modified_since":%d, "if_unmodified_since":%d }`,
+		c.IsFresh, c.NoCache, c.NoTransform, c.FreshnessLifetime, c.CanRevalidate, c.MustRevalidate, c.LastModified.Unix(), c.Expires.Unix(), c.Date.Unix(), c.LocalDate.Unix(),
+		c.ETag, c.IfNoneMatchValue, c.IfMatchValue, c.IfModifiedSinceTime.Unix(), c.IfUnmodifiedSinceTime.Unix())
 }
 
 // DocumentFromHTTPResponse returns an HTTPDocument from the provided HTTP Response and Body
