dedc959128729e38129f756ff86e4cd69b17bfe4
James Ranson <james_ranson@cable.comcast.com>
2019-09-18 11:07:47 -0600

    add config context middleware

---
+++ internal/util/middleware/config_context.go
@@ -0,0 +1,28 @@
+/**
+* Copyright 2018 Comcast Cable Communications Management, LLC
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+ */
+
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/Comcast/trickster/internal/config"
+	"github.com/Comcast/trickster/internal/util/context"
+)
+
+// WithConfigContext ...
+func WithConfigContext(o *config.OriginConfig, c *config.CachingConfig, p *config.ProxyPathConfig, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		next.ServeHTTP(w, r.WithContext(context.WithConfigs(r.Context(), o, c, p)))
+	})
+}
