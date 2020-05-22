cfc0ee820acf1b59b5e1b1827fcb336cf0706593
James Ranson <james_ranson@cable.comcast.com>
2020-05-22 13:59:12 -0600

    bump version to 1.0.4

--- cmd/trickster/main.go
+++ cmd/trickster/main.go
@@ -44,7 +44,7 @@ var (
 
 const (
 	applicationName    = "trickster"
-	applicationVersion = "1.0.3"
+	applicationVersion = "1.0.4"
 )
 
 // Package main is the main package for the Trickster application
