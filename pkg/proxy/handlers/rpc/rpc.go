14507acde20f6ee1d773de36478be1a41b371ac7
James Ranson <james_ranson@cable.comcast.com>
2020-08-31 15:45:08 -0600

    add missing license banners (#484)

--- pkg/proxy/handlers/rpc/rpc.go
+++ pkg/proxy/handlers/rpc/rpc.go
@@ -1,13 +1,29 @@
+/*
+ * Copyright 2018 Comcast Cable Communications Management, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package rpc
 
 import (
 	"net/http"
 	"net/url"
 
-	co "github.com/tricksterproxy/trickster/pkg/cache/options"
-	"github.com/tricksterproxy/trickster/pkg/cache/registration"
 	oo "github.com/tricksterproxy/trickster/pkg/backends/options"
 	rpc "github.com/tricksterproxy/trickster/pkg/backends/reverseproxycache"
+	co "github.com/tricksterproxy/trickster/pkg/cache/options"
+	"github.com/tricksterproxy/trickster/pkg/cache/registration"
 	"github.com/tricksterproxy/trickster/pkg/routing"
 
 	"github.com/gorilla/mux"
