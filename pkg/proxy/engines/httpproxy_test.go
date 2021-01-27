badbe2182cc4cb068651bb3f0a403c1625289661
Samuel García Martínez <samuelgma@inditex.com>
2021-01-27 13:21:56 +0100

    fix TestClockOffsetWarning test for the CET timezone

--- pkg/proxy/engines/httpproxy_test.go
+++ pkg/proxy/engines/httpproxy_test.go
@@ -131,7 +131,7 @@ func TestProxyRequestBadGateway(t *testing.T) {
 func TestClockOffsetWarning(t *testing.T) {
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add(headers.NameDate, time.Now().Add(-1*time.Hour).Format(http.TimeFormat))
+		w.Header().Add(headers.NameDate, time.Now().UTC().Add(-1*time.Hour).Format(http.TimeFormat))
 		w.WriteHeader(200)
 	}
 	s := httptest.NewServer(http.HandlerFunc(handler))
