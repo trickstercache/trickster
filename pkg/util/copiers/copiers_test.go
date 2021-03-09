298966dc0b749038c43d081d6565701e503ec19b
James Ranson <james@ranson.org>
2021-03-09 16:22:02 -0700

    test coverage

--- pkg/util/copiers/copiers_test.go
+++ pkg/util/copiers/copiers_test.go
@@ -18,6 +18,43 @@ package copiers
 
 import "testing"
 
+func TestCopyBytes(t *testing.T) {
+
+	m1 := CopyBytes(nil)
+	if m1 != nil {
+		t.Error("expected nil map")
+	}
+
+	m := []byte("test")
+
+	m2 := CopyBytes(m)
+	if len(m2) != 4 {
+		t.Errorf("expected %d got %d", 4, len(m2))
+	}
+	if m2[0] != 't' {
+		t.Errorf("expected %c got %c", 't', m2[0])
+	}
+}
+
+func TestCopyInterfaces(t *testing.T) {
+
+	m1 := CopyInterfaces(nil)
+	if m1 != nil {
+		t.Error("expected nil map")
+	}
+
+	m := []interface{}{1}
+
+	m2 := CopyInterfaces(m)
+	if len(m2) != 1 {
+		t.Errorf("expected %d got %d", 4, len(m2))
+	}
+	if m2[0].(int) != 1 {
+		t.Errorf("expected %d got %d", 1, m2[0])
+	}
+
+}
+
 func TestCopyStrings(t *testing.T) {
 
 	m1 := CopyStrings(nil)
@@ -56,6 +93,44 @@ func TestCopyStringLookup(t *testing.T) {
 
 	if v != expected {
 		t.Errorf("expected %s got %s", expected, v)
+	}
+
+}
+
+func TestCopyLookup(t *testing.T) {
+
+	m1 := CopyLookup(nil)
+	if m1 != nil {
+		t.Error("expected nil map")
+	}
+
+	m := map[string]interface{}{"test": nil}
+	m2 := CopyLookup(m)
+
+	v, ok := m2["test"]
+	if !ok {
+		t.Errorf("expected true got %t", ok)
+	}
+
+	if v != nil {
+		t.Errorf("expected nil got %v", v)
+	}
+
+}
+
+func TestLookupFromStrings(t *testing.T) {
+
+	m1 := LookupFromStrings(nil)
+	if m1 != nil {
+		t.Error("expected nil map")
+	}
+
+	m := []string{"test", "trickster"}
+	m2 := LookupFromStrings(m)
+
+	_, ok := m2["test"]
+	if !ok {
+		t.Errorf("expected true got %t", ok)
 	}
 
 }
