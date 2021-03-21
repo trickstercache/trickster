9247d63e72ac720ef5b0222c25781492edbe94a9
James Ranson <james@ranson.org>
2021-03-21 06:53:00 -0600

    cache options test coverage to 100%
    Signed-off-by: James Ranson <james@ranson.org>

--- pkg/cache/options/options_test.go
+++ pkg/cache/options/options_test.go
@@ -16,7 +16,14 @@
 
 package options
 
-import "testing"
+import (
+	"strings"
+	"testing"
+
+	"github.com/tricksterproxy/trickster/pkg/cache/providers"
+	strutil "github.com/tricksterproxy/trickster/pkg/util/strings"
+	"github.com/tricksterproxy/trickster/pkg/util/yamlx"
+)
 
 func TestNew(t *testing.T) {
 	o := New()
@@ -39,3 +46,119 @@ func TestCloneAndEqual(t *testing.T) {
 	}
 
 }
+
+func TestSetDefaults(t *testing.T) {
+
+	// SetDefaults(metadata yamlx.KeyLookup,
+	//activeCaches map[string]interface{})
+	// ([]string, error) {
+
+	o := New()
+	l := Lookup{"default": o}
+	_, err := l.SetDefaults(nil, nil)
+	if err != nil {
+		t.Error()
+	}
+
+	kl, err := yamlx.GetKeyList(testYAML)
+	if err != nil {
+		t.Error(err)
+	}
+
+	o.Provider = "Redis"
+	o.ProviderID = providers.Redis
+	l = Lookup{"default": o}
+
+	ac := strutil.Lookup{"default": nil}
+	lw, err := l.SetDefaults(kl, ac)
+	if err != nil {
+		t.Error()
+	}
+
+	if len(lw) != 1 {
+		t.Errorf("expected %d got %d", 1, len(lw))
+	}
+
+	ty := strings.Replace(
+		strings.Replace(testYAML,
+			"client_type: standard", "client_type: sentinel", -1),
+		"endpoints: [ 127.0.0.1:6839 ]", "endpoint: 127.0.0.1:6839", -1)
+
+	kl, err = yamlx.GetKeyList(ty)
+	if err != nil {
+		t.Error(err)
+	}
+
+	l = Lookup{"default": o}
+	o.Redis.ClientType = "sentinel"
+	ac = strutil.Lookup{"default": nil}
+	lw, err = l.SetDefaults(kl, ac)
+	if err != nil {
+		t.Error()
+	}
+	if len(lw) != 1 {
+		t.Errorf("expected %d got %d", 1, len(lw))
+	}
+
+	l = Lookup{"default": o}
+	o.Index.MaxSizeBackoffBytes = 16384
+	o.Index.MaxSizeBytes = 1
+	_, err = l.SetDefaults(kl, ac)
+	if err != errMaxSizeBackoffBytesTooBig {
+		t.Error(err)
+	}
+
+	l = Lookup{"default": o}
+	o.Index.MaxSizeBackoffBytes = 16384
+	o.Index.MaxSizeBytes = 32768
+	o.Index.MaxSizeBackoffObjects = 32768
+	o.Index.MaxSizeObjects = 16384
+
+	_, err = l.SetDefaults(kl, ac)
+	if err != errMaxSizeBackoffObjectsTooBig {
+		t.Error(err)
+	}
+
+}
+
+const testYAML = `
+caches:
+  default:
+    provider: redis
+    redis:
+      client_type: standard
+      protocol: tcp
+      endpoints: [ 127.0.0.1:6839 ]
+      client_type: sentinel
+      sentinel_master: 127.0.0.1:6839
+      password: '********'
+      db: trickster
+      max_retries: 3
+      min_retry_backoff_ms: 2000
+      max_retry_backoff_ms: 4000
+      dial_timeout_ms: 2000
+      read_timeout_ms: 1000
+      write_timeout_ms: 3000
+      pool_size: 16
+      min_idle_conns: 16
+      max_conn_age_ms: 16
+      pool_timeout_ms: 16
+      idle_timeout_ms: 16
+      idle_check_frequency_ms: 16
+    filesystem:
+      cache_path: /tmp/trickster
+    bbolt:
+      filename: trickster.bbolt.db
+      bucket: trickster
+    badger:
+      directory: /tmp/trickster
+      value_directory: /tmp/trickster
+    index:
+      reap_interval_ms: 2000
+      flush_interval_ms: 2000
+      max_size_bytes: 1
+      max_size_backoff_bytes: 16384
+      max_size_objects: 4096
+      max_size_backoff_objects: 24
+
+`
