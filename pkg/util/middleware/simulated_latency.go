6371e65c8e624f58fb49ac440dbca987cf6186ae
James Ranson <james@ranson.org>
2021-09-05 22:23:12 -0600

    refactor simulated latnency + add response header
    Signed-off-by: James Ranson <james@ranson.org>

--- pkg/util/middleware/simulated_latency.go
+++ pkg/util/middleware/simulated_latency.go
@@ -13,13 +13,15 @@ func processSimulatedLatency(w http.ResponseWriter, minMS, maxMS int) {
 	if (minMS == 0 && maxMS == 0) || (minMS < 0 || maxMS < 0) {
 		return
 	}
+	var ms int64
 	if minMS >= maxMS {
-		w.Header().Set(latencyHeaderName, strconv.Itoa(minMS)+"ms")
-		time.Sleep(time.Duration(minMS) * time.Millisecond)
+		ms = int64(minMS)
+	} else {
+		ms = (rand.Int63() % int64(maxMS-minMS)) + int64(minMS)
+	}
+	if ms <= 0 {
 		return
 	}
-	diff := int64(maxMS - minMS)
-	v := (rand.Int63() % diff) + int64(minMS)
-	time.Sleep(time.Duration(v) * time.Millisecond)
-	w.Header().Set(latencyHeaderName, strconv.FormatInt(v, 10)+"ms")
+	time.Sleep(time.Duration(ms) * time.Millisecond)
+	w.Header().Set(latencyHeaderName, strconv.FormatInt(ms, 10)+"ms")
 }
