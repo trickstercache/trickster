360e1d1c0fe1919fdfb287b9498c0e25dbd181d4
James Ranson <james@ranson.org>
2023-01-22 12:58:44 -0700

    replace fmt.Print* with logger functions
    Signed-off-by: James Ranson <james@ranson.org>

--- pkg/proxy/handlers/purge.go
+++ pkg/proxy/handlers/purge.go
@@ -1,14 +1,15 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/trickstercache/trickster/v2/cmd/trickster/config"
 	"github.com/trickstercache/trickster/v2/pkg/backends"
 	"github.com/trickstercache/trickster/v2/pkg/checksum/md5"
+	"github.com/trickstercache/trickster/v2/pkg/observability/logging"
 	"github.com/trickstercache/trickster/v2/pkg/proxy/headers"
+	"github.com/trickstercache/trickster/v2/pkg/proxy/request"
 )
 
 // PurgeHandleFunc purges an object from a cache based on key.
@@ -42,18 +43,18 @@ func PurgeKeyHandleFunc(conf *config.Config, from *backends.Backends) func(http.
 
 func PurgePathHandlerFunc(conf *config.Config, from *backends.Backends) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Println("Purging by path")
+		rsc := request.GetResources(req)
 		purgeFrom := req.URL.Query().Get("backend")
 		purgePath := req.URL.Query().Get("path")
 		if purgeFrom == "" || purgePath == "" {
-			fmt.Println("Failed to get backend/path args")
+			logging.Warn(rsc.Logger, "failed to get backend/path args", nil)
 			w.Header().Set(headers.NameContentType, headers.ValueTextPlain)
 			w.Header().Set(headers.NameCacheControl, headers.ValueNoCache)
 			w.WriteHeader(http.StatusBadRequest)
 			w.Write([]byte("Usage: " + config.DefaultPurgePathHandlerPath + "?backend={backend}&path={path}"))
 			return
 		}
-		fmt.Printf("%s %s\n", purgeFrom, purgePath)
+		logging.Debug(rsc.Logger, "purging cache item", logging.Pairs{"backend": purgeFrom, "path": purgePath})
 		fromBackend := from.Get(purgeFrom)
 		if fromBackend == nil {
 			w.Header().Set(headers.NameContentType, headers.ValueTextPlain)
