7e0d4e26d7eca8e5efff694a1d24fc16c08f3bcf
Christopher Randles <randles.chris@gmail.com>
2025-03-31 09:45:48 -0400

    feat: [ci] add docker image release on "on tag" workflow (#753)
    fix: [ci] fix docker image release "on main" workflow
    
    Signed-off-by: Chris Randles <randles.chris@gmail.com>

---
+++ hack/release-sha256.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+CHECKSUM_FILE=$1
+BUILD_SUBDIR=$2
+PROGVER=$3
+BIN_DIR=$4
+
+rm ${CHECKSUM_FILE} > /dev/null 2> /dev/null && touch ${CHECKSUM_FILE}
+
+pushd $BUILD_SUBDIR > /dev/null
+sha256sum trickster-$PROGVER.tar.gz > $(basename $CHECKSUM_FILE)
+popd > /dev/null
+
+RSF="$(realpath ${CHECKSUM_FILE})" && for file in ${BIN_DIR}/*; do
+    if [[ "$(basename $file)" == "sha256sum.txt" ]]; then
+        continue
+    fi
+    pushd $(dirname $file) > /dev/null
+    sha256sum "$(basename $file)" >> ${RSF}
+    popd > /dev/null
+done
+
+cat $CHECKSUM_FILE
+
