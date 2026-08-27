2cacb6d357dda144053b02069e4a219f136bbbc7
Elia Renzoni <elia.renzoni03@gmail.com>
2026-08-27 18:54:08 +0200

    fix: failing test
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- pkg/routing/routing_test.go
+++ pkg/routing/routing_test.go
@@ -259,7 +259,9 @@ func TestRegisterProxyRoutesALB(t *testing.T) {
 		t.Fatalf("Could not load configuration: %s", err.Error())
 	}
 
-	conf.Backends["default"].ALBOptions = &options.Options{MechanismName: names.MechanismTSM, OutputFormat: providers.Prometheus}
+	opts := &options.Options{MechanismName: names.MechanismTSM}
+	opts.TSMOptions.OutputFormat = providers.Prometheus
+	conf.Backends["default"].ALBOptions = opts
 
 	caches := registry.LoadCachesFromConfig(conf)
 	defer registry.CloseCaches(caches)
