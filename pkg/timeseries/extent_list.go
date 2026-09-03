eeb0f6bf4b0500db6fdccb427eafea38611f5d51
shiyudesu <ming051017@outlook.com>
2026-09-03 10:45:03 +0800

    refactor: move empty extent trimming to ExtentList
    Signed-off-by: shiyudesu <ming051017@outlook.com>

--- pkg/timeseries/extent_list.go
+++ pkg/timeseries/extent_list.go
@@ -348,6 +348,20 @@ func (el ExtentList) Clone() ExtentList {
 	return c
 }
 
+// TrimEmptyExtents returns a copy of the ExtentList with empty Extents removed.
+func (el ExtentList) TrimEmptyExtents() ExtentList {
+	trimmed := make(ExtentList, len(el))
+	var cursor int
+	for _, extent := range el {
+		if extent == (Extent{}) {
+			continue
+		}
+		trimmed[cursor] = extent
+		cursor++
+	}
+	return trimmed[:cursor]
+}
+
 // CloneRange returns a perfect copy of the ExtentList, cloning only the
 // Extents in the provided index range (upper-bound exclusive)
 func (el ExtentList) CloneRange(start, end int) ExtentList {
